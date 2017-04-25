c80f43e24898d5954365891355888ec75292e97b
Peter K <pkjar16@student.aau.dk>
2017-04-25 14:24:09 +0200

    Add files via upload

---
+++ sql2.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <mysql/mysql.h>
+
+#define DATABASE_NAME  "SM"
+#define DATABASE_USERNAME "root"
+#define DATABASE_PASSWORD "kopkaffe"
+MYSQL *mysql1;
+
+//*****************************************
+//*****************************************
+//********** CONNECT TO DATABASE **********
+//*****************************************
+//*****************************************
+void mysql_connect (void)
+{
+     //initialize MYSQL object for connections
+ mysql1 = mysql_init(NULL);
+
+     if(mysql1 == NULL)
+     {
+         fprintf(stderr, "ABB : %s\n", mysql_error(mysql1));
+         return;
+     }
+
+     //Connect to the database
+     if(mysql_real_connect(mysql1, "localhost", DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME, 0, NULL, 0) == NULL)
+     {
+      fprintf(stderr, "%s\n", mysql_error(mysql1));
+     }
+     else
+     {
+         printf("Database connection successful.\r\n");
+     }
+}
+
+
+int main(int argc, char **argv)
+{
+ mysql_connect();
+        return 0;
+}
